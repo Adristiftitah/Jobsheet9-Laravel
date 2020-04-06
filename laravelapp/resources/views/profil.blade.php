4f66bfa0f9818260bd3b30db6d138f3d21a8b501
Adristiftitah <adristiiftitahyuniar@gmail.com>
2020-04-06 09:32:49 +0700

    Jobsheet9-Laravel

---
+++ laravelapp/resources/views/profil.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>	Profile Saya </title>
+</head>
+<body>
+		<h1>Profil Saya</h1>
+		<p>Perkenalkan Nama saya Adristi Iftitah Yuniar, saya seorang mahasiswa jurusan Teknik Informatika</p>
+</body>
+</html>
